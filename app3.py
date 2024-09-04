52e3130b50453735e39b6f2e6c8a40b1bb4c78da
Karla Rodriguez <rodriguezkarla1901@gmail.com>
2024-09-03 21:24:24 -0500

    final

--- app3.py
+++ app3.py
@@ -1,3 +1,5 @@
+"""Respuestas puntos 1 al 3"""
+
 def diccionario_alumnos():
     alumnos = {}
     while True:
@@ -36,9 +38,62 @@ alumnos = diccionario_alumnos()
 promedio = promedio_notas(alumnos)
 mejor_alumno = alumno_con_mejor_nota(alumnos)
 
-"""Respuestas puntos 1 al 3"""
+
 print ("Alumnos registrados y sus notas:", alumnos)
 print(f"El promedio de las notas del equipo es: {promedio:.2f}")
 print(f"El alumno con la mejor nota es: {mejor_alumno}")
+"""
+
+"""Punto 4"""
+"""
+def ingresar_palabras():
+    diccionario = {}
+    while True:
+        palabra = input("Ingrese una palabra (o 'salir' para terminar): ")
+        if palabra.lower() == 'salir':
+            break
+        definicion = input("Ingrese la definición de la palabra: ")
+        diccionario[palabra] = definicion
+    return diccionario
+
+def mostrar_diccionario_palabras(diccionario):
+    print("\nDiccionario de palabras y sus definiciones:")
+    for palabra, definicion in diccionario.items():
+        print(f"{palabra}: {definicion}")
+
+diccionario_palabras = ingresar_palabras()
+mostrar_diccionario_palabras(diccionario_palabras)
 
 
+"""Punton 4 y 5"""
+
+def ingresar_palabras():
+    diccionario = {}
+    while True:
+        palabra = input("Ingrese una palabra (o 'salir' para terminar): ")
+        if palabra.lower() == 'salir':
+            break
+        definicion = input("Ingrese la definición de la palabra: ")
+        diccionario[palabra] = definicion
+    return diccionario
+
+def mostrar_diccionario_palabras(diccionario):
+    print("\nDiccionario de palabras y sus definiciones:")
+    for palabra, definicion in diccionario.items():
+        print(f"{palabra}: {definicion}")
+
+def buscar_palabra(diccionario, palabra):
+    definicion = diccionario.get(palabra)
+    if definicion:
+        print(f"Definición de '{palabra}': {definicion}")
+    else:
+        print(f"La palabra '{palabra}' no se encuentra en el diccionario.")
+
+diccionario_palabras = ingresar_palabras()
+mostrar_diccionario_palabras(diccionario_palabras)
+
+while True:
+    palabra_a_buscar = input("\nIngrese una palabra para buscar (o 'salir' para terminar): ")
+    if palabra_a_buscar.lower() == 'salir':
+        break
+    buscar_palabra(diccionario_palabras, palabra_a_buscar)
